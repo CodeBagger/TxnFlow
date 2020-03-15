f0bf828234ef43f909f3663a0c65b788febe5e43
CodeBagger <mikekopelowitz@gmail.com>
2020-03-15 08:31:30 +0200

    Add files via upload

--- TxnFlow.Logic/BusinessRules/CanDeletePerson.cs
+++ TxnFlow.Logic/BusinessRules/CanDeletePerson.cs
@@ -10,26 +10,22 @@ namespace TxnFlow.Logic.BusinessRules
 {
     public class CanDeletePerson : IRule
     {
-        private readonly IPerson _person;
-        private string _failureMessage;
-        private CanDeletePerson() { }
-        public CanDeletePerson(IPerson person)
+        IPerson _person;
+        CanDeletePerson(IPerson person)
         {
             _person = person;
+        }
+        public string FailureMessage
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public bool Validate()
         {
-            var accountsCount = _person.Accounts.Count;
-            var validate = accountsCount == 0;
-            if (!validate)
-            {
-                _failureMessage =
-                    $"Person {_person.name} still has {accountsCount} linked. Please remove all accounts before deleting.";
-            }
-            return validate;
+            throw new NotImplementedException();
         }
-
-        public string FailureMessage => _failureMessage;
     }
 }
---
+++ TxnFlow.Logic/BusinessRules/CanSavePerson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TxnFlow.Data;
+using TxnFlow.Interfaces.Data_Access;
+using TxnFlow.Interfaces.Entities;
+using TxnFlow.Interfaces.Rules;
+
+namespace TxnFlow.Logic.BusinessRules
+{
+    public class CanSavePerson : IRule
+    {
+        IPerson _person;
+        public CanSavePerson(IPerson person)
+        {
+            _person = person;
+            FailureMessage = "";
+        }
+        public string FailureMessage
+        {
+            get;set;
+        }
+
+        public bool Validate()
+        {
+            IPerson _alreadyExistingPerson;
+            IDataRepositoryModel DRM = new DataRepositoryModel();
+            _alreadyExistingPerson = DRM.FindByIdNumber(_person.id_number);
+            if (_alreadyExistingPerson != null)
+            {
+                FailureMessage = "Person Already Exists";
+                return false;
+                
+                
+            }
+            return true;
+        }
+    }
+}
