8dffa0db09a4cc9765dcf246e109b5aa95525cb1
CodeBagger <mikekopelowitz@gmail.com>
2020-03-15 08:28:04 +0200

    Add files via upload

--- TxnFlow.Infrastructure/DeleteException.cs
+++ TxnFlow.Infrastructure/DeleteException.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TxnFlow.Infrastructure
 {
     public class DeleteException : Exception
     {
-        public DeleteException(string message):base(message)
+        public DeleteException(string message) : base(message)
         {
         }
     }
--- TxnFlow.Infrastructure/SaveException.cs
+++ TxnFlow.Infrastructure/SaveException.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TxnFlow.Infrastructure
 {
     public class SaveException : Exception
     {
-        public SaveException(string message):base(message)
+        public SaveException(string message) : base(message)
         {
         }
     }
